46e68d0b5b3b8ee8b4f95049ee4d7b595298d514
purnangshu13 <128771956+purnangshu13@users.noreply.github.com>
2023-04-25 20:19:01 +0530

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,23 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class Client {
+
+	public  static void main (String [] args) {
+	  boolean flag = true;
+	  Scanner sc = new Scanner (System.in);
+	  while(flag) {
+		System.out.println("Enter the input or q to quit ::");
+		String input = sc.next();
+		if (input.equals ("q")) {
+			flag = false;
+			continue;
+	    }
+		boolean result= BalancedBrackets.isBalanced(input);
+		System.out.println("Input is balanced ? "+ result);
+	  }
+	    sc.close();
+	}
+  }
+
